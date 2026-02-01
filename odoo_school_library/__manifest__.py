ec8db5d1c8d2ec0b17407590fcac2c36f0012f97
Bondarchuk Viacheslav <bondarchuk.viacheslav@gmail.com>
2026-02-01 09:23:21 +0200

    Initial commit

---
+++ odoo_school_library/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Odoo school library Bondarchuk',
+    'version': '19.0.1.1.0',
+    'author': 'Bondarchuk V',
+    'category': 'Customizations',
+    'website': 'http://www.bondarchuk.com',
+    'license': 'OPL-1',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': []
+    },
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
