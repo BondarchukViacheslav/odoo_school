13b2db09995dceb86cb5e7edbb6a0eeb80cd644a
Bondarchuk Viacheslav <bondarchuk.viacheslav@gmail.com>
2026-02-02 18:45:17 +0200

    Виконано

--- hr_hospital/models/hr_hospital_doctor.py
+++ hr_hospital/models/hr_hospital_doctor.py
@@ -5,7 +5,7 @@ from odoo import models, fields
 _logger = logging.getLogger(__name__)
 
 
-class HRHospitalDooctor(models.Model):
+class HRHospitalDoctor(models.Model):
     _name = 'hr.hospital.doctor'
     _description = 'Doctor'
 
